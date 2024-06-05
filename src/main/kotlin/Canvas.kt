416f851c94e245d7bf46ffb3a886afac36c646e1
milenamrugala <milena.mrugala@icloud.com>
2024-06-05 18:45:35 +0200

    Circle class

--- src/main/kotlin/Canvas.kt
+++ src/main/kotlin/Canvas.kt
@@ -1,2 +1,17 @@
 class Canvas {
+    private var shapes = mutableListOf<Shape>()
+
+    fun add(shape: Shape) {
+        shapes.add(shape)
+    }
+
+    fun remove(shape: Shape) {
+        shapes.remove(shape)
+    }
+
+    fun paint() {
+        for (shape in shapes) {
+            shape.draw()
+        }
+    }
 }
--- src/main/kotlin/Circle.kt
+++ src/main/kotlin/Circle.kt
@@ -1,2 +1,19 @@
-class Circle {
+class Circle(
+    private var center: Point,
+    private var radius: Line,
+    private var color: String,
+    private var filled: Boolean
+) : Shape(color, filled) {
+    override fun draw() {
+        println("Draw circle: ${center.getX()}, ${center.getY()} with radius: $radius, color: $color, filled: $filled")
+    }
+
+    override fun erase() {
+        println("Erase circle: ${center.getX()}, ${center.getY()} with radius: $radius")
+    }
+
+    override fun transform() {
+        println("Transform circle: ${center.getX()}, ${center.getY()} with radius: $radius")
+    }
+
 }
