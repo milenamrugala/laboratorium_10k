4436e5811fad936ba7fd9eab1e1ed381b821cf70
milenamrugala <milena.mrugala@icloud.com>
2024-06-05 18:46:00 +0200

    Line and Point class

--- src/main/kotlin/Line.kt
+++ src/main/kotlin/Line.kt
@@ -1,2 +1,23 @@
-class Libe {
+class Line(
+    private var start: Point,
+    private var end: Point,
+    private var color: String,
+    private var filled: Boolean
+) : Shape(color, filled) {
+    override fun draw() {
+        println("Draw line: (${start.getX()}, ${start.getY()}) to (${end.getX()}, ${end.getY()}, color: $color, filled: $filled")
+    }
+
+    override fun erase() {
+        println("Erase line: (${start.getX()}, ${start.getY()}) to (${end.getX()}, ${end.getY()})")
+    }
+
+    override fun transform() {
+        println("Transform line: (${start.getX()}, ${start.getY()}) to (${end.getX()}, ${end.getY()})")
+    }
+
+    override fun toString(): String {
+        return "$start, $end"
+    }
+
 }
--- src/main/kotlin/Point.kt
+++ src/main/kotlin/Point.kt
@@ -1,7 +1,41 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+class Point(
+    private var x: Double,
+    private var y: Double,
+    private var color: String,
+    private var filled: Boolean
+) : Shape(color, filled) {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    override fun draw() {
+        println("Draw: ($x, $y), color: $color, filled: $filled")
+    }
+
+    override fun erase() {
+        println("Erase: ($x, $y)")
+    }
+
+    override fun transform() {
+        println("Transform: ($x, $y)")
+    }
+
+    fun getX(): Double {
+        return x
+    }
+
+    fun getY(): Double {
+        return y
+    }
+
+    fun setX(x: Double) {
+        this.x = x
+    }
+
+    fun setY(y: Double) {
+        this.y = y
+    }
+
+    override fun toString(): String {
+        return "$x, $y"
+    }
+
+
 }
